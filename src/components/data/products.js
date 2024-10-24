ef96273e1a50cd6eee59540654533d75c6c4c5cd
hoainam <hoainambld2003@gmail.com>
2024-10-24 16:10:40 +0700

    .

---
+++ src/components/data/products.js
@@ -0,0 +1,18 @@
+// src/data/products.js
+export const productsData = [
+  {
+    id: 1,
+    name: "Sản phẩm 1",
+    image: ".../public/images/usersImage/test.jpg", // Đường dẫn hình ảnh sản phẩm
+    shortDescription: "Mô tả ngắn cho sản phẩm 1.",
+    price: 25.0,
+  },
+  {
+    id: 2,
+    name: "Sản phẩm 2",
+    image: "/images/product2.jpg",
+    shortDescription: "Mô tả ngắn cho sản phẩm 2.",
+    price: 30.0,
+  },
+  // Thêm nhiều sản phẩm khác
+];
